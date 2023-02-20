55423b1e3e2bc45a574f5a383fc694d14eddc847
Hritik Jaiswal <76836429+Hritik-Jaiswal@users.noreply.github.com>
2023-02-20 21:23:02 +0530

    fixed connection issues

--- client/src/contract/index.js
+++ client/src/contract/index.js
@@ -1,5 +1,5 @@
 import contract from './AVAXGods.json'
 
-export const ADDRESS = '0xC11a2F607c793f0c021141De1AdBcAEDdA6D46E1'
+export const ADDRESS = '0xEE960817FD07315a644B836f03d51CEB193b40bE'
 
 export const { abi: ABI } = contract; // allows our front-end side to call the functions we have created in our smart contracts.
